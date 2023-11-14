02a7362b2bde6649f7fc17ff8bc5bbef96d95f1a
mystery000 <lkostyrka17@gmail.com>
2023-11-15 00:31:47 +0900

    Remove unused code

---
+++ SWAPI_POC/Configuration/CacheConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace SWAPI_POC.Configuration
+{
+    public static class CacheConfig
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+        }
+    }
+}
